f1a6677097c2f9709740d7938c6c025dd421a688
MifitMy <170085130@qq.com>
2019-08-22 10:41:54 +0800

    Initial Commit

---
+++ XMReSwiftSingleModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XMReSwiftSingleModule
+//
+//  Created by 梁小迷 on 22/8/19.
+//  Copyright © 2019年 mifit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
